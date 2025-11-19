771155d234be3c721fca00b5429e16c7cf9312a5
victor <victor@gmail.com>
2025-11-19 21:33:39 +0800

    created crud connection to database

---
+++ backend/crud/user_crud.py
@@ -0,0 +1,61 @@
+from config_db_connection import Config
+
+conn = Config().create_connection()
+
+def get_username(username):
+    """
+    Fetch a user by username. Used to check if a user exists during registration.
+    """
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = %s"
+    cursor.execute(query, (username))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+def create_user(user_data):
+    """
+    Create a new user in the database.
+    """
+    cursor = conn.cursor()
+    insert_query = "INSERT INTO User (username, user_password, nickname) VALUES (%s, %s, %s)"
+    cursor.execute(insert_query, (user_data['username'], user_data['password'], user_data['nickname']))
+    conn.commit()
+    cursor.close()
+    return
+
+def login_user(username, password):
+    """
+    Authenticate a user with username and password.
+    """
+    cursor = conn.cursor()
+    query = "SELECT * FROM users WHERE username = %s AND user_password = %s"
+    cursor.execute(query, (username, password))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+def get_user_points(user_id):
+    """
+    Retrieve the points of a user by their ID.
+    """
+    cursor = conn.cursor()
+    query = "SELECT points FROM users WHERE user_id = %s"
+    cursor.execute(query, (user_id))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+def modify_user_settings(user_id, modified_settings):
+    return
+
+def modify_user_points(user_id, points):
+    """
+    Update the points of a user by their ID.
+    """
+    cursor = conn.cursor()
+    query = "UPDATE User SET points = %s WHERE user_id = %s"
+    cursor.execute(query, (points, user_id))
+    conn.commit()
+    cursor.close()
+    return
